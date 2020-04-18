fe4d35cc055b44f0e884a730b476b86ee4ba620e
PedroMiranda <pbragamiranda@gmail.com>
2020-04-18 14:57:44 -0300

    messaging

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show", as: :task
+  get 'tasks', to: 'tasks#index'
+  get "tasks/:id/edit", to: "tasks#edit", as: :edittask
+  patch "tasks/:id", to: "tasks#update", as: :updatetask
+  get 'tasks/new', to: 'tasks#new', as: :newtask
+  post 'tasks', to: 'tasks#create'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :deletetask
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
